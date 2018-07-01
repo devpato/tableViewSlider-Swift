c25d9c329479708547630bb9e7a7b51db9b33d49
Patricio Vargas <patricio_vargasgonzalez@csx.com>
2018-06-30 21:27:25 -0400

    numbers dispalying on table

--- timesTable/ViewController.swift
+++ timesTable/ViewController.swift
@@ -9,19 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var slider: UISlider!
+
+    @IBOutlet weak var table: UITableView!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 50
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = "hi"
+        cell.textLabel?.text = String(Int(slider.value * 20) * (indexPath.row + 1))
+        
         return cell
     }
     
-    
-    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,7 +37,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     @IBAction func sliderChanged(_ sender: Any) {
-        print(slider.value)
+        table.reloadData()
     }
     
    
